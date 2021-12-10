bdb364a2650221728f2cb5f35bc0f569e8609fb8
Mathias Reitinger <mathias.reitinger@loop0.org>
2021-12-10 21:01:40 +0100

    setup GitHub actions

--- runtest.sh
+++ runtest.sh
@@ -8,6 +8,8 @@ p2p=$(cat $testcase | raku python2.raku | perl)
 
 if [[ "X${p27}" = "X${p2p}" ]]; then
     echo "${testcase} ... OK"
+    exit 0
 else
     echo "${testcase} ... FAILED"
+    exit 1
 fi
