a07dd5b390ee095f6988b1644cd459231e499f66
Mathias Reitinger <mathias.reitinger@loop0.org>
2021-12-15 18:46:29 +0100

    output better messages if a interpreter exits with >0

--- runtest.sh
+++ runtest.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-set -e
+set -o pipefail
 
 testcase=$1
 
-p27=$(cat $testcase | python2.7)
-p2p=$(cat $testcase | raku python2.raku | perl)
+p27=$(cat $testcase | python2.7 2>&1)
+if [[ $? -gt 0 ]]; then
+    echo "${testcase} ... FAILED (python interpreter)"
+    exit 1
+fi
+
+p2p=$(cat $testcase | raku python2.raku 2>&1 | perl 2>&1)
+if [[ $? -gt 0 ]]; then
+    echo "${testcase} ... FAILED (raku/perl interpreter)"
+    exit 1
+fi
 
 if [[ "X${p27}" = "X${p2p}" ]]; then
     echo "${testcase} ... OK"
