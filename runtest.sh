eafa99236e3c44865229e2c828f28a52491a12ba
Mathias Reitinger <mathias.reitinger@loop0.org>
2021-12-22 16:11:01 +0100

    allow script names as parameter stead of reading from STDIN
    Grammar::Debugger needs STDIN to be 'free'

--- runtest.sh
+++ runtest.sh
@@ -3,13 +3,13 @@ set -o pipefail
 
 testcase=$1
 
-p27=$(cat $testcase | python2.7 2>&1)
+p27=$(python2.7 $testcase 2>&1)
 if [[ $? -gt 0 ]]; then
     echo "${testcase} ... FAILED (python interpreter)"
     exit 1
 fi
 
-p2p=$(cat $testcase | raku python2.raku 2>&1 | perl 2>&1)
+p2p=$(raku python2.raku $testcase 2>&1 | perl 2>&1)
 if [[ $? -gt 0 ]]; then
     echo "${testcase} ... FAILED (raku/perl interpreter)"
     exit 1
