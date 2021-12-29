edffa33997196e5ddff16b33d1f2d80d9768a31c
Stefan Seifert <nine@detonation.org>
2021-12-29 09:37:25 +0100

    Have runtest.sh output TAP so it can be run in a TAP harness

--- runtest.sh
+++ runtest.sh
@@ -3,22 +3,27 @@ set -o pipefail
 
 testcase=$1
 
+echo "1..3"
 p27=$(python2.7 $testcase 2>&1)
 if [[ $? -gt 0 ]]; then
-    echo "${testcase} ... FAILED (python interpreter)"
+    echo "not ok 1 # ${testcase} ... FAILED (python interpreter)"
     exit 1
+else
+    echo "ok 1"
 fi
 
 p2p=$(raku python2.raku $testcase 2>&1 | perl 2>&1)
 if [[ $? -gt 0 ]]; then
-    echo "${testcase} ... FAILED (raku/perl interpreter)"
+    echo "not ok 2 # ${testcase} ... FAILED (raku/perl interpreter)"
     exit 1
+else
+    echo "ok 2"
 fi
 
 if [[ "X${p27}" = "X${p2p}" ]]; then
-    echo "${testcase} ... OK"
+    echo "ok 3 # ${testcase}"
     exit 0
 else
-    echo "${testcase} ... FAILED"
+    echo "not ok 3 # ${testcase}"
     exit 1
 fi
