fbf20c012526116e23cd6f10e668f1546144bdc9
Mathias Reitinger <mathias.reitinger@loop0.org>
2022-01-20 22:58:14 +0100

    better try/except check

--- t/output-comparison/try-except-basic.py
+++ t/output-comparison/try-except-basic.py
@@ -1,17 +1,24 @@
 try:
     print x
 except:
-    print 'error'
+    print 'error 1'
 
 if 1:
     try:
         print x
     except:
-        print 'error'
+        print 'error 2'
+
+try:
+    x = 1
+    print x
+except:
+    print 'error 3'
+
 
 try:
     print x
 except:
-    print 'error'
+    print 'error 4'
 finally:
     print 'done'
