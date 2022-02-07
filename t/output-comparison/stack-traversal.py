b3c99c6c01fd2578395cda8dc95fb6f6901845db
Mathias Reitinger <mathias.reitinger@loop0.org>
2022-02-07 18:35:12 +0100

    better stack traversal test

--- t/output-comparison/stack-traversal.py
+++ t/output-comparison/stack-traversal.py
@@ -1,17 +1,29 @@
 x = 1
 
-def func():
-    x = 2
-    print x
+print x
 
-def funcx(x):
-    print x
+def func():
+    try:
+        print x
+    except:
+        print 'access to undefined x failed'
+
+    print 'func() body executed'
     x = 2
     print x
 
 print x
 
-func()
-funcx(x)
+def funcx(x):
+    print 'funcx() body executed'
+    print x
+    x = 3
+    print x
 
+print x
+
+func()
+print x
+
+funcx(x)
 print x
