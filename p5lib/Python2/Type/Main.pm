240c1b46168ee71104ed2a77501f29d3c5445807
Mathias Reitinger <mathias.reitinger@loop0.org>
2022-07-19 14:25:47 +0200

    die() instead of exit() in error handler
    exit also exits our parent when embedding

--- p5lib/Python2/Type/Main.pm
+++ p5lib/Python2/Type/Main.pm
@@ -33,79 +33,79 @@ sub __run__ {
 sub __handle_python_exception__ {
     my ($self, $error) = @_;
 
-        my $message        = $error->message;
+    my $output;
 
-        my $start_position;
-        my $end_position;
-        while (my $frame = $error->__trace__->next_frame) {
-            next unless $frame->package =~ m/^(Python2::Type|python_class_main)/;
+    my $message        = $error->message;
 
-            if ($frame->filename =~ m/^___position_(\d+)_(\d+)___$/) {
-                $start_position = $1;
-                $end_position   = $2;
-            }
+    my $start_position;
+    my $end_position;
+    while (my $frame = $error->__trace__->next_frame) {
+        next unless $frame->package =~ m/^(Python2::Type|python_class_main)/;
+
+        if ($frame->filename =~ m/^___position_(\d+)_(\d+)___$/) {
+            $start_position = $1;
+            $end_position   = $2;
         }
+    }
 
-        # unable to get a decent internal position - do the best we can and output a
-        # perl stack trace
-        unless ($start_position and $end_position) {
-            die $error->__trace__->as_string;
-        }
+    # unable to get a decent internal position - do the best we can and output a
+    # perl stack trace
+    unless ($start_position and $end_position) {
+        die $error->__trace__->as_string;
+    }
 
-        my @input_as_lines = split(/\n/, $self->__source__);
+    my @input_as_lines = split(/\n/, $self->__source__);
 
-        my $failed_at_line = () = substr($self->__source__, 0, $start_position) =~ /\n/g;
-        $failed_at_line++;
+    my $failed_at_line = () = substr($self->__source__, 0, $start_position) =~ /\n/g;
+    $failed_at_line++;
 
-        my $failed_position_in_line =
-            $failed_at_line > 1
-                ?
-                    (
-                        $start_position  # total position (in charaters) where the execution failed
+    my $failed_position_in_line =
+        $failed_at_line > 1
+            ?
+                (
+                    $start_position  # total position (in charaters) where the execution failed
 
-                        # ignore all characters from preceeding lines
-                        - length(join("\n", @input_as_lines[0 .. $failed_at_line - 2]))
+                    # ignore all characters from preceeding lines
+                    - length(join("\n", @input_as_lines[0 .. $failed_at_line - 2]))
 
-                        # ignore last \n
-                        -1
-                    )
-                :
-                    $start_position;
+                    # ignore last \n
+                    -1
+                )
+            :
+                $start_position;
 
+    $output .= "Execution failed at line $failed_at_line ($start_position, $end_position)\n\n";
 
-        print STDERR "Execution failed at line $failed_at_line ($start_position, $end_position)\n\n";
+    # output preceeding line, if present
+    $output .= sprintf("%5i | %s\n",
+        $failed_at_line - 1,
+        $input_as_lines[$failed_at_line - 2],
+    ) if defined $input_as_lines[$failed_at_line - 2];
 
-        # output preceeding line, if present
-        say STDERR sprintf("%5i | %s",
-            $failed_at_line - 1,
-            $input_as_lines[$failed_at_line - 2],
-        ) if defined $input_as_lines[$failed_at_line - 2];
+    # output line with syntax error
+    $output .= sprintf("%5i | %s\n",
+        $failed_at_line,
+        $input_as_lines[$failed_at_line - 1],
+    );
 
-        # output line with syntax error
-        say STDERR sprintf("%5i | %s",
-            $failed_at_line,
-            $input_as_lines[$failed_at_line - 1],
-        );
+    $output .= '        ' . ' ' x $failed_position_in_line;
+    $output .= '^' x ($end_position - $start_position - 1) . " - $message\n";
 
-        print STDERR '        ' . ' ' x $failed_position_in_line;
-        print '^' x ($end_position - $start_position - 1) . " - $message\n";
+    # output subsequent line, if present
+    $output .= sprintf("%5i | %s\n",
+        $failed_at_line + 1,
+        $input_as_lines[$failed_at_line - 0],
+    ) if defined $input_as_lines[$failed_at_line - 0];
 
-        # output subsequent line, if present
-        say STDERR sprintf("%5i | %s",
-            $failed_at_line + 1,
-            $input_as_lines[$failed_at_line - 0],
-        ) if defined $input_as_lines[$failed_at_line - 0];
+    $output .= "\n";
 
-        say STDERR '';
+    $output .= "Internal stack trace:\n";
 
-        say STDERR 'Internal stack trace:';
+    while (my $frame = $error->__trace__->next_frame) {
+        $output .= sprintf(" - %s(%s)\n", $frame->subroutine, join(', ', map { substr( $_, 0, 20 ) } $frame->args));
+    }
 
-        while (my $frame = $error->__trace__->next_frame) {
-            say STDERR sprintf(' - %s(%s)', $frame->subroutine, join(', ', $frame->args));
-        }
-
-        exit 1;
-
+    die $output;
 }
 
 sub __type__ { return 'pyobject'; }
