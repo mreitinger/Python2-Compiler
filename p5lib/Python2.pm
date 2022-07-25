0d1d16f7a1e101927883f9ba96248b663879b933
Sascha Kotanko <sko@atikon.com>
2022-07-25 14:28:52 +0200

    implement round

--- p5lib/Python2.pm
+++ p5lib/Python2.pm
@@ -138,7 +138,31 @@ our $builtins = [
                 my $retval = sum(@plist) + $start_value->__tonative__;
 
                 return \Python2::Type::Scalar::Num->new($retval);
-            }
+            },
+
+            'round' => sub {
+                pop(@_); # default named arguments hash
+
+                my ($value, $precision) = @_;
+
+                $precision ||= Python2::Type::Scalar::Num->new(0);
+
+                die Python2::Type::Exception->new('TypeError', 'a number is required as value')
+                    unless ($value->__class__ eq 'Python2::Type::Scalar::Num');
+
+                die Python2::Type::Exception->new('TypeError', 'a number is required as precision')
+                    unless ($precision->__type__ eq 'int');
+
+                my $retval = sprintf(
+                    sprintf('%%.%if', $precision->__tonative__),
+                    $value->__tonative__
+                );
+
+                $retval = "$retval.0" if $retval =~ m/^\d+$/;
+                $retval =~ s/([1-9])0+$/$1/;
+
+                return \Python2::Type::Scalar::Num->new($retval);
+            },
         }
     ]
 ];
---
+++ t/output-comparison-python-interpreter/function-round.py
@@ -0,0 +1,15 @@
+print round(1.99)
+print round(1.5)
+print round(1.4)
+
+print round(1.99, 1)
+print round(1.5, 1)
+print round(1.4, 1)
+
+print round(1.99, 2)
+print round(1.5, 2)
+print round(1.4, 2)
+
+print round(1.99, 9)
+print round(1.5, 9)
+print round(1.4, 9)
