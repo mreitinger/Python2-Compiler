c31e0478e27a0b2070334da43207e8cf08da0976
Mathias Reitinger <mathias.reitinger@loop0.org>
2022-07-28 15:35:04 +0200

    trow Python2::Type::Exception for not implemented methods
    provides better diagnostics at runtime

--- p5lib/Python2/Type.pm
+++ p5lib/Python2/Type.pm
@@ -12,13 +12,13 @@ use Scalar::Util qw/ refaddr /;
 use constant { PARENT => 0, ITEMS => 1 };
 
 sub __print__   { return shift->__str__; }
-sub __str__     { ...; }
+sub __str__     { die Python2::Type::Exception->new('NotImplementedError', '__str__ for ' . shift->__type__); }
 sub __dump__    { warn Dumper(shift); }
 sub __class__   { ref(shift); }
-sub __type__    { ...; }
+sub __type__    { die Python2::Type::Exception->new('NotImplementedError', '__type__ for ' . ref(shift)); }
 
-sub __cmp__     { ...; }
-sub __eq__      { ...; } # ==
+sub __cmp__     { die Python2::Type::Exception->new('NotImplementedError', '__cmp__ between ' . shift->__type__ . ' and ' . shift->__type__); }
+sub __eq__      { die Python2::Type::Exception->new('NotImplementedError', '__eq__ between ' . shift->__type__ . ' and ' . shift->__type__); } # ==
 
 # we implement this using __hasattr__ instead of (ab)using __getattr__ in case we need more
 # fine control when interfacing with perl 5 objects.
@@ -38,10 +38,10 @@ sub __ne__      {
 }
 
 
-sub __lt__      { ...; } # <
-sub __gt__      { ...; } # >
-sub __le__      { ...; } # <=
-sub __ge__      { ...; } # >=
+sub __lt__      { die Python2::Type::Exception->new('NotImplementedError', '__lt__ between ' . shift->__type__ . ' and ' . shift->__type__); } # <
+sub __gt__      { die Python2::Type::Exception->new('NotImplementedError', '__gt__ between ' . shift->__type__ . ' and ' . shift->__type__); } # >
+sub __le__      { die Python2::Type::Exception->new('NotImplementedError', '__le__ between ' . shift->__type__ . ' and ' . shift->__type__);; } # <=
+sub __ge__      { die Python2::Type::Exception->new('NotImplementedError', '__ge__ between ' . shift->__type__ . ' and ' . shift->__type__);; } # >=
 
 # is - used for our X is Y implementation, python2 has no explicit __is__
 sub __is__  {
