cc45acf5a9f0167ae429591e840366d3e8961590
Mathias Reitinger <mathias.reitinger@loop0.org>
2022-08-29 09:31:23 +0200

    die when named arguments get passed to coderefs - we don't handle them yet

--- p5lib/Python2/Type/PerlSub.pm
+++ p5lib/Python2/Type/PerlSub.pm
@@ -17,7 +17,16 @@ sub new {
 sub __call__ {
     my ($self, $pstack, @argument_list) = @_;
 
-    pop(@argument_list); # unused named arguments hash
+    # last argument is the hashref with named arguments
+    my $named_arguments = pop(@argument_list);
+
+    # we don't support named arguments but still expect the empty hash - just here to catch bugs
+    die Python2::Type::Exception->new('NotImplementedError', "expected named arguments hash when calling perl5 coderef")
+        unless ref($named_arguments) eq 'HASH';
+
+    die Python2::Type::Exception->new('NotImplementedError', "named arguments not supported when calling perl5 coderef")
+        if scalar(%$named_arguments);
+
 
     foreach my $argument (@argument_list) {
         $argument = $argument->__tonative__;
