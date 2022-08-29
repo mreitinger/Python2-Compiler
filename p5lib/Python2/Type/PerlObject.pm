a33b5a132cb11abbc8d2ca1dbf1882a57d56db70
Mathias Reitinger <mathias.reitinger@loop0.org>
2022-08-29 09:15:53 +0200

    better diagnostics for method calls on perl objects

--- p5lib/Python2/Type/PerlObject.pm
+++ p5lib/Python2/Type/PerlObject.pm
@@ -85,7 +85,11 @@ sub AUTOLOAD {
     # last argument is the hashref with named arguments
     my $named_arguments = pop(@argument_list);
 
-    die Python2::Type::Exception->new('NotImplementedError', 'named arguments not supported when calling perl5 methods')
+    # we don't support named arguments but still expect the empty hash - just here to catch bugs
+    die Python2::Type::Exception->new('NotImplementedError', "expected named arguments hash when calling perl5 method $requested_method on " . ref($self->{object}))
+        unless ref($named_arguments) eq 'HASH';
+
+    die Python2::Type::Exception->new('NotImplementedError', "named arguments not supported when calling perl5 methods $requested_method on " . ref($self->{object}))
         if scalar(%$named_arguments);
 
     # convert all 'Python' objects to native representations
