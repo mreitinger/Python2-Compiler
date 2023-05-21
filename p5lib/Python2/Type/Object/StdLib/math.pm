a0bd9e801a14fbdae107faeb451402f411ae59b6
Mathias Reitinger mathias.reitinger@rootprompt.at <mathias.reitinger@rootprompt.at>
2023-05-21 12:50:07 +0200

    get started on stdlib/math
    only math.floor() and math.ceil() implemented so far

---
+++ p5lib/Python2/Type/Object/StdLib/math.pm
@@ -0,0 +1,44 @@
+package Python2::Type::Object::StdLib::math;
+
+use base qw/ Python2::Type::Object::StdLib::base /;
+
+use v5.26.0;
+use warnings;
+use strict;
+use POSIX ();
+
+sub new {
+    my ($self) = @_;
+
+    my $object = bless({
+        stack => [$Python2::builtins],
+    }, $self);
+
+    return $object;
+}
+
+sub floor {
+    my $self = shift @_;
+    pop @_; # unused named arguments hash
+    my $value = shift @_;
+
+    die Python2::Type::Exception->new('TypeError', 'floor() expects a number, got ' . (defined $value ? $value->__type__ : 'nothing'))
+        unless defined $value and ref($value) eq 'Python2::Type::Scalar::Num';
+
+    return \Python2::Type::Scalar::Num->new( POSIX::floor($value->__tonative__) );
+}
+
+sub ceil {
+    my $self = shift @_;
+    pop @_; # unused named arguments hash
+    my $value = shift @_;
+
+    die Python2::Type::Exception->new('TypeError', 'ceil() expects a number, got ' . (defined $value ? $value->__type__ : 'nothing'))
+        unless defined $value and ref($value) eq 'Python2::Type::Scalar::Num';
+
+    return \Python2::Type::Scalar::Num->new( POSIX::ceil($value->__tonative__) );
+}
+
+
+
+1;
---
+++ t/output-comparison-python-interpreter/stdlib-match.py
@@ -0,0 +1,5 @@
+import math
+
+# int() because we don't match pythons float/int distinction perfectly
+print int(math.floor(2.3))
+print int(math.ceil(2.3))
