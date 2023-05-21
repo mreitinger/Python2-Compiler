22ce627283583b167a4035eed22f811e72c2b143
Mathias Reitinger mathias.reitinger@rootprompt.at <mathias.reitinger@rootprompt.at>
2023-05-21 12:27:45 +0200

    support key parameter for sorted()

--- p5lib/Python2/Builtin/Sorted.pm
+++ p5lib/Python2/Builtin/Sorted.pm
@@ -7,8 +7,25 @@ use strict;
 sub __name__ { 'sorted' }
 sub __call__ {
     shift @_; # $self - unused
+    my $named_arguments = pop @_;
 
-    \Python2::Type::List->new( sort { $a->__tonative__ cmp $b->__tonative__ } $_[0]->ELEMENTS );
+    my $key = exists $named_arguments->{key} ? ${ $named_arguments->{key} } : undef;
+
+    die Python2::Type::Exception->new('TypeError', 'key passed to sorted is not callable (does not support __call__)')
+        if defined $key and not $key->can('__call__');
+
+    return $key
+        ?   \Python2::Type::List->new( sort {
+                ${ $key->__call__($a, bless({}, 'Python2::NamedArgumentsHash')) }->__tonative__
+                cmp
+                ${ $key->__call__($b, bless({}, 'Python2::NamedArgumentsHash')) }->__tonative__
+            } $_[0]->ELEMENTS )
+
+        :   \Python2::Type::List->new(
+                sort { $a->__tonative__ cmp $b->__tonative__
+            } $_[0]->ELEMENTS )
+
+        ;
 };
 
 1;
--- t/output-comparison-python-interpreter/function-sorted.py
+++ t/output-comparison-python-interpreter/function-sorted.py
@@ -1,3 +1,10 @@
 print sorted([5, 4, 1, 2, 3])
 print sorted(['b', 'c', 'd', 'a', 'f'])
 print sorted([5, 2, 'a', 'c', 'b'])
+
+def t(b):
+    return b.lower()
+
+print sorted("Test string A b C".split())
+print sorted("Test string A b C".split(), key=t)
+print sorted("Test string A b C".split(), key=lambda s: s.lower())
