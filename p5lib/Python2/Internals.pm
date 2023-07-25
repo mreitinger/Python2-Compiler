0a66645edbe7cee19285b6424da1366e5f3bebf4
Mathias Reitinger mathias.reitinger@rootprompt.at <mathias.reitinger@rootprompt.at>
2023-07-25 08:46:11 +0200

    add str*int

--- p5lib/Python2/Internals.pm
+++ p5lib/Python2/Internals.pm
@@ -155,6 +155,8 @@ my $arithmetic_operations = {
 
         if ($left->isa('Python2::Type::Scalar::Num') and $right->isa('Python2::Type::Scalar::Num')) {
             return \Python2::Type::Scalar::Num->new($left->__tonative__ * $right->__tonative__);
+        } elsif ($left->isa('Python2::Type::Scalar::String') and $right->isa('Python2::Type::Scalar::Num')) {
+            return \Python2::Type::Scalar::String->new($left->__tonative__ x int($right->__tonative__));
         } elsif (($left->__type__ eq 'list') and ($right->__type__ eq 'int')) {
             my $count = $right->__tonative__;
             $count = 0 if $count < 0;
