b8debd1b2dda9878ca87e5c04218822565458d3b
Mathias Reitinger mathias.reitinger@rootprompt.at <mathias.reitinger@rootprompt.at>
2023-07-25 09:11:57 +0200

    fix explicit float/float division

--- p5lib/Python2/Builtin/Float.pm
+++ p5lib/Python2/Builtin/Float.pm
@@ -11,12 +11,19 @@ sub __call__ {
     shift @_; # $self - unused
 
     my $val = $_[0]->__tonative__;
+
     # insert leading zero as python does, if needed
     $val =~ s/^()\./0\./;
-    die Python2::Type::Exception->new('ValueError',
-        'invalid literal for float(): ' . $val)
+
+    die Python2::Type::Exception->new('ValueError', 'invalid literal for float(): ' . $val)
         unless (looks_like_number($val));
-    \Python2::Type::Scalar::Num->new($val);
+
+    # we cheat and only have a single Type::Num instead of dedicated int/float types
+    # append '.0' to make Num return float if float() would otherwise return a plain
+    # integer
+    return $val =~ m/^\d+$/
+        ? \Python2::Type::Scalar::Num->new($val . '.0')
+        : \Python2::Type::Scalar::Num->new($val)
 }
 
 1;
---
+++ t/output-comparison-python-interpreter/arithmetic-division-int-cast-to-float.py
@@ -0,0 +1,7 @@
+print float(660)
+print float(1920)
+print float(660) / float(1920)
+print float(660) / float(1920) * 100
+print float(660) / float(1920) * 100 + 0.5
+print round(float(660) / float(1920) * 100 + 0.5)
+
